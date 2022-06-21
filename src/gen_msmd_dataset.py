5908d87c55d2d73f816047c7deffc89586c05d38
woodenonez <parkerben9508@gmail.com>
2022-06-21 10:00:16 +0200

    fix data id bug

--- src/gen_msmd_dataset.py
+++ src/gen_msmd_dataset.py
@@ -6,7 +6,7 @@ from util import utils_data
 print("Generate synthetic segmentation dataset.")
 
 root_dir = Path(__file__).resolve().parents[1]
-save_path = os.path.join(root_dir, 'Data/MSMD_1t10_test2/') # save in folder
+save_path = os.path.join(root_dir, 'Data/MSMD_20_test2/') # save in folder
 
 past = 4
 minT = 1
@@ -15,8 +15,8 @@ sim_time_per_scene = 5 # times
 # index_list = [1,2,3,4,5,6,7,8,9] # each index should have at least 30 trajectories
 index_list = [10,11,12] # new scene for test
 
-utils_data.save_MSMD_data(index_list, save_path, sim_time_per_scene)
-print('CSV records for each index generated.')
+# utils_data.save_MSMD_data(index_list, save_path, sim_time_per_scene)
+# print('CSV records for each index generated.')
 
 utils_data.gather_all_data_trajectory(save_path, past, maxT=maxT, minT=minT) # go through all the obj folders and put them together in one CSV
 print('Final CSV generated!')
--- src/gen_sid_dataset.py
+++ src/gen_sid_dataset.py
@@ -6,16 +6,16 @@ from util import utils_data
 print("Generate synthetic segmentation dataset.")
 
 root_dir = Path(__file__).resolve().parents[1]
-save_path = os.path.join(root_dir, 'Data/SID_20_train/') # save in folder
+save_path = os.path.join(root_dir, 'Data/SID_1t10_train/') # save in folder
 
 past = 4
-minT = 20
-maxT = 20
+minT = 1
+maxT = 10
 sim_time_per_scene = 10 # times, 10,2
 index_list = list(range(1,11)) # list(range(1,11)) # 1~10 # [1,3,5] a simple crossing
 
 utils_data.save_SID_data(index_list, save_path, sim_time_per_scene)
 print('CSV records for each index generated.')
 
-utils_data.gather_all_data_position(save_path, past, maxT=maxT, minT=minT) # go through all the obj folders and put them together in one CSV
+utils_data.gather_all_data_trajectory(save_path, past, maxT=maxT, minT=minT) # go through all the obj folders and put them together in one CSV
 print('Final CSV generated!')
